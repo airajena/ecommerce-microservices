3876e108bc1467617c7cded0d238e4cc87a882a4
Aira Jena <airajena.hack@gmail.com>
2025-02-10 18:19:51 +0530

    feat: Add product request dto

--- ProductService/src/main/java/com/ecommerce/ProductService/service/ProductService.java
+++ ProductService/src/main/java/com/ecommerce/ProductService/service/ProductService.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Product {
+public class ProductService {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
