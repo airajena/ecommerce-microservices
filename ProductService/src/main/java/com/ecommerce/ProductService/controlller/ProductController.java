70ed4be3af3101296eb854740c5111a6b23e64f5
Aira Jena <airajena.hack@gmail.com>
2025-02-10 18:22:31 +0530

    feat: Add product controller

--- ProductService/src/main/java/com/ecommerce/ProductService/controlller/ProductController.java
+++ ProductService/src/main/java/com/ecommerce/ProductService/controlller/ProductController.java
@@ -1,5 +1,23 @@
 package com.ecommerce.ProductService.controlller;
 
+import com.ecommerce.ProductService.model.ProductRequest;
+import com.ecommerce.ProductService.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
 
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
+        long productId = productService.addProduct(productRequest);
+        return new ResponseEntity<>(productId, HttpStatus.CREATED);
+    }
 }
