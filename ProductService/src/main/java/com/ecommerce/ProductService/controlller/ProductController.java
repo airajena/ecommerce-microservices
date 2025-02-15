4a4319ac642610b4d41694318fd3063647dcef8d
Aira Jena <airajena.hack@gmail.com>
2025-02-15 09:31:33 +0530

    feat: Add get product by id endpoint

--- ProductService/src/main/java/com/ecommerce/ProductService/controlller/ProductController.java
+++ ProductService/src/main/java/com/ecommerce/ProductService/controlller/ProductController.java
@@ -2,13 +2,11 @@ package com.ecommerce.ProductService.controlller;
 
 import com.ecommerce.ProductService.model.ProductRequest;
 
+import com.ecommerce.ProductService.model.ProductResponse;
 import com.ecommerce.ProductService.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -27,5 +25,11 @@ public class ProductController {
         return new ResponseEntity<>(productId, HttpStatus.CREATED);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId) {
+        ProductResponse productResponse
+                = productService.getProductById(productId);
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
+    }
 
 }
