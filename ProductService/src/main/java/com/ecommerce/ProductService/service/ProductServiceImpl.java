8b8975335a173d92c0cb66a0684af7cb3250834e
Aira Jena <airajena.hack@gmail.com>
2025-02-15 09:33:46 +0530

    feat: Add get product by id service

--- ProductService/src/main/java/com/ecommerce/ProductService/service/ProductServiceImpl.java
+++ ProductService/src/main/java/com/ecommerce/ProductService/service/ProductServiceImpl.java
@@ -2,26 +2,27 @@ package com.ecommerce.ProductService.service;
 
 import com.ecommerce.ProductService.entity.Product;
 import com.ecommerce.ProductService.model.ProductRequest;
+import com.ecommerce.ProductService.model.ProductResponse;
 import com.ecommerce.ProductService.repository.ProductRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 public class ProductServiceImpl implements ProductService {
 
+    private final ProductRepository productRepository;
+
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
-
-    private ProductRepository productRepository;
 
     @Override
     public long addProduct(ProductRequest productRequest) {
         log.info("Adding Product..");
 
-        Product product
-                = Product.builder()
+        Product product = Product.builder()
                 .productName(productRequest.getName())
                 .quantity(productRequest.getQuantity())
                 .price(productRequest.getPrice())
@@ -29,7 +30,20 @@ public class ProductServiceImpl implements ProductService {
 
         productRepository.save(product);
 
-        log.info("Product Created");
+        log.info("Product Created with ID: {}", product.getProductId());
         return product.getProductId();
+    }
+
+    @Override
+    public ProductResponse getProductById(long productId) {
+        log.info("Fetching product for productId: {}", productId);
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Product not found for productId: " + productId));
+
+        ProductResponse productResponse = new ProductResponse();
+        BeanUtils.copyProperties(product, productResponse);
+
+        return productResponse;
     }
 }
