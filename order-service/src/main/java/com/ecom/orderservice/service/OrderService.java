873f52e07009142a61d074339fc113b600caf824
Aira Jena <airajena.hack@gmail.com>
2025-02-15 10:40:43 +0530

    feat: Add order service

--- order-service/src/main/java/com/ecom/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ecom/orderservice/service/OrderService.java
@@ -3,7 +3,7 @@ package com.ecom.orderservice.service;
 import com.ecom.orderservice.model.OrderRequest;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface OrderService {
     long placeOrder(OrderRequest orderRequest);
 
--- order-service/src/main/java/com/ecom/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/ecom/orderservice/service/OrderServiceImpl.java
@@ -1,5 +1,38 @@
 package com.ecom.orderservice.service;
 
-public class OrderServiceImpl {
+import com.ecom.orderservice.entity.Order;
+import com.ecom.orderservice.model.OrderRequest;
+import com.ecom.orderservice.repository.OrderRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@Log4j2
+public class OrderServiceImpl implements OrderService {
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    private OrderRepository orderRepository;
+
+    @Override
+    public long placeOrder(OrderRequest orderRequest) {
+        log.info("Creating Order with Status CREATED");
+        Order order = Order.builder()
+                .amount(orderRequest.getTotalAmount())
+                .orderStatus("CREATED")
+                .productId(orderRequest.getProductId())
+                .orderDate(Instant.now())
+                .quantity(orderRequest.getQuantity())
+                .build();
+
+        order = orderRepository.save(order);
+
+        log.info("Order Places successfully with Order Id: {}", order.getId());
+        return order.getId();
+    }
+
 
 }
