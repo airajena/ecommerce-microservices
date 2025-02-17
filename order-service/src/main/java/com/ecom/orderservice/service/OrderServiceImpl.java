ff7e68637cea9bc533e82215c1c76e0373d49af4
Aira Jena <airajena.hack@gmail.com>
2025-02-17 11:50:39 +0530

    feat: Add product service feign client

--- order-service/src/main/java/com/ecom/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/ecom/orderservice/service/OrderServiceImpl.java
@@ -1,25 +1,35 @@
 package com.ecom.orderservice.service;
 
 import com.ecom.orderservice.entity.Order;
+import com.ecom.orderservice.external.client.ProductService;
 import com.ecom.orderservice.model.OrderRequest;
 import com.ecom.orderservice.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 
 @Service
 @Log4j2
 public class OrderServiceImpl implements OrderService {
-    public OrderServiceImpl(OrderRepository orderRepository) {
+
+    private final OrderRepository orderRepository;
+    private final ProductService productService;
+
+    public OrderServiceImpl(OrderRepository orderRepository, ProductService productService) {
         this.orderRepository = orderRepository;
+        this.productService = productService;
     }
 
-    private OrderRepository orderRepository;
-
     @Override
+    @Transactional
     public long placeOrder(OrderRequest orderRequest) {
         log.info("Creating Order with Status CREATED");
+
+        // Reduce product quantity before placing an order
+        productService.reduceQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
+
         Order order = Order.builder()
                 .amount(orderRequest.getTotalAmount())
                 .orderStatus("CREATED")
@@ -30,9 +40,7 @@ public class OrderServiceImpl implements OrderService {
 
         order = orderRepository.save(order);
 
-        log.info("Order Places successfully with Order Id: {}", order.getId());
+        log.info("Order Placed successfully with Order Id: {}", order.getId());
         return order.getId();
     }
-
-
 }
